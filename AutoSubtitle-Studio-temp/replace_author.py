f54b3706f80fea1f5094103ff8af1b910276700e
DroneTechTI <dronetechticino@gmail.com>
2026-02-09 10:05:14 +0100

    .

--- AutoSubtitle-Studio-temp/replace_author.py
+++
@@ -1,8 +0,0 @@
-# replace_author.py
-def commit_callback(commit):
-    if commit.author_name == b"claude":
-        commit.author_name = b"DroneTechTI"
-        commit.author_email = b"dronetechticino@gmail.com"
-    if commit.committer_name == b"claude":
-        commit.committer_name = b"DroneTechTI"
-        commit.committer_email = b"dronetechticino@gmail.com"
